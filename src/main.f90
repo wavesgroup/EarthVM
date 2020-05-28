b6da4a29103ced007ca0bc66170690e91126e660
milancurcic <caomaco@gmail.com>
2020-05-28 19:34:08 -0400

    use simpler variable names

--- src/main.f90
+++ src/main.f90
@@ -9,7 +9,7 @@ program main
 
   implicit none
 
-  type(earthvm_model_type) :: atmosphere_model, ocean_model
+  type(earthvm_model_type) :: atmosphere, ocean
   type(datetime) :: start_time, stop_time, time
   integer :: local_pet
 
@@ -19,33 +19,34 @@ program main
   call earthvm_initialize()
   local_pet = earthvm_get_local_pet()
 
-  atmosphere_model = earthvm_model_type('wrf', start_time, stop_time, &
-                                        45, set_wrf_services)
-  ocean_model = earthvm_model_type('hycom', start_time, stop_time, &
-                                   60, set_hycom_services)
+  atmosphere = earthvm_model_type('wrf', start_time, stop_time, &
+                                  45, set_wrf_services)
+  ocean = earthvm_model_type('hycom', start_time, stop_time, &
+                             60, set_hycom_services)
 
-  call atmosphere_model % initialize()
-  call ocean_model % initialize()
-  call ocean_model % force(atmosphere_model)
+  call atmosphere % initialize()
+  call ocean % initialize()
+  call ocean % force(atmosphere)
 
   time = start_time
   do
 
     ! run atmosphere for one time step
-    if (atmosphere_model % get_current_time() <= time) then
+    if (atmosphere % get_current_time() <= time) then
       if (local_pet == 0) print *, &
-        time % strftime('%Y-%m-%d %H:%M:%S'), ': Running atmosphere model'
-      call atmosphere_model % run()
-      call atmosphere_model % write_to_netcdf()
+        time % strftime('%Y-%m-%d %H:%M:%S'), ': Running atmosphere'
+      call atmosphere % run()
+      call atmosphere % force(ocean)
+      call atmosphere % write_to_netcdf()
     end if
 
     ! run ocean for one time step
-    if (ocean_model % get_current_time() <= time) then
+    if (ocean % get_current_time() <= time) then
       if (local_pet == 0) print *, &
-        time % strftime('%Y-%m-%d %H:%M:%S'), ': Running ocean model'
-      call ocean_model % run()
-      call ocean_model % write_to_netcdf()
-      call ocean_model % force(atmosphere_model)
+        time % strftime('%Y-%m-%d %H:%M:%S'), ': Running ocean'
+      call ocean % run()
+      call ocean % write_to_netcdf()
+      call ocean % force(atmosphere)
     end if
 
     ! advance master clock
