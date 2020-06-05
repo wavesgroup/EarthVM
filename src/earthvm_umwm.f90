ac3ddb8f726abec23502473b94906a2db35c0f17
milancurcic <caomaco@gmail.com>
2020-06-05 17:16:53 -0400

    creating distgrid and grid for the wave component

--- src/earthvm_umwm.f90
+++ src/earthvm_umwm.f90
@@ -6,7 +6,8 @@ module earthvm_umwm
                           get_field_values, set_field_values
   use earthvm_events
   use earthvm_io, only: write_grid_to_netcdf
-  use earthvm_state, only: earthvm_get_mpicomm
+  use earthvm_state, only: earthvm_get_mpicomm, earthvm_get_local_pet, &
+                           earthvm_get_pet_count
   use umwm_top, only: umwm_initialize, umwm_run, umwm_finalize
 
   implicit none
@@ -38,6 +39,9 @@ contains
 
 
   subroutine model_init(gridded_component, import_state, export_state, clock, rc)
+
+    use umwm_module, only: mm, nm, istart, iend, mi, ni, lon, lat, mask
+
     type(ESMF_GridComp) :: gridded_component
     type(ESMF_State) :: import_state, export_state
     type(ESMF_Clock) :: clock
@@ -46,8 +50,35 @@ contains
     integer, intent(out) :: rc
 
     type(ESMF_Field), allocatable :: fields(:)
+    integer :: ips, ipe, jps, jpe
+    integer :: local_pet, pet_count
+
+    local_pet = earthvm_get_local_pet()
+    pet_count = earthvm_get_pet_count()
 
     call umwm_initialize()
+
+    if (mm >= nm) then
+      ! row-major remapping
+      ips = mi(istart)
+      ipe = mi(iend)
+      jps = 1
+      jpe = nm
+      if (local_pet == 0) ips = ips - 1
+      if (local_pet == pet_count - 1) ipe = ipe + 1
+    else if (mm < nm) then
+      ! column-major remapping
+      ips = 1
+      ipe = mm
+      jps = ni(istart)
+      jpe = ni(iend)
+      if (local_pet == 0) jps = jps - 1
+      if (local_pet == pet_count - 1) jpe = jpe + 1
+    end if
+
+    distgrid = create_distgrid([ips, jps], [ipe, jpe], [1, 1], [mm, nm])
+    grid = create_grid(distgrid, 'UMWM grid', lon, lat, mask)
+    call write_grid_to_netcdf(grid, 'umwm_grid.nc')
 
     rc = ESMF_SUCCESS
   end subroutine model_init
