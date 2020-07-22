8ded4a29569204828addaa3227a27d265e48a176
milancurcic <caomaco@gmail.com>
2020-07-22 13:54:39 -0400

    update environment script

--- set_env.sh
+++ set_env.sh
@@ -4,18 +4,19 @@ export WRF=`pwd`/wrf-4.2
 export UMWM=`pwd`/umwm/src
 export HYCOM=`pwd`/hycom-2.3.01
 
-export HDF5=`pwd`/netcdf
-export NETCDF=`pwd`/netcdf
+#export HDF5=`pwd`/netcdf
+#export NETCDF=`pwd`/netcdf
 
 # ESMF configuration
 export ESMF_OS=Linux
-export ESMF_DIR=`pwd`/esmf-8.0.0
-export ESMF_COMPILER=gfortran
+export ESMF_DIR=`pwd`/esmf
+export ESMF_COMPILER=intel
 export ESMF_ABI=64
 export ESMF_BOPT=O
 export ESMF_COMM=openmpi
 export ESMF_PTHREADS=OFF
 export ESMF_OPENMP=OFF
+export ESMF_YAMLCPP=OFF
 
 export ESMF_LIB=${ESMF_DIR}/lib/lib${ESMF_BOPT}/${ESMF_OS}.${ESMF_COMPILER}.${ESMF_ABI}.${ESMF_COMM}.default
 export ESMF_INCLUDE=${ESMF_DIR}/mod/mod${ESMF_BOPT}/${ESMF_OS}.${ESMF_COMPILER}.${ESMF_ABI}.${ESMF_COMM}.default
